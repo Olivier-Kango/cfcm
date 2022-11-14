727731638119ee8ab1f76bcf295d477db28cbbe4
Olivier-Kango <olivierkango@gmail.com>
2022-11-13 18:32:40 -0800

    intall react-router-dom

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders CFCM', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/CFCM/i);
-  expect(linkElement).toBeInTheDocument();
-});
