e6da60098b0c8e6b89153db4be33f016d46fff2e
Olivier-Kango <olivierkango@gmail.com>
2022-11-13 18:33:05 -0800

    install React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      CFCM
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
