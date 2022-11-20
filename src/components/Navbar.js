1b6c12186a97f0d506d19ee0f2c8820218839dd7
Olivier-Kango <olivierkango@gmail.com>
2022-11-19 18:50:37 -0800

    Add toggler option for each link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg bg-light">
+  <nav className="navbar navbar-expand-lg bg-white">
     <div className="container-fluid">
-      <Link to="/" className="navbar-brand">Logos</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
+      <Link to="/" className="navbar-brand">
+        <i>C.F.C.M</i>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span
+          className="navbar-toggler-icon"
+        />
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to="/" className="nav-link active">Accueil</Link>
+            <Link to="/" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Accueil</Link>
           </li>
           <li className="nav-item">
-            <Link to="/formation" className="nav-link active">Formation</Link>
+            <Link to="/formation" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Formation</Link>
           </li>
           <li className="nav-item">
-            <Link to="/services" className="nav-link active">Nos Services</Link>
+            <Link to="/services" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Nos Services</Link>
           </li>
           <li className="nav-item">
-            <Link to="/qui-sommes-nous" className="nav-link active">Qui sommes-nous?</Link>
+            <Link to="/qui-sommes-nous" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Qui sommes-nous?</Link>
           </li>
           <li className="nav-item">
-            <Link to="/medias" className="nav-link active">Médias</Link>
+            <Link to="/medias" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Médias</Link>
           </li>
           <li className="nav-item active">
-            <Link to="/direct" className="nav-link active">Direct</Link>
+            <Link to="/direct" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Direct</Link>
           </li>
           <li className="nav-item active">
-            <Link to="/contact" className="nav-link active">Contact</Link>
+            <Link to="/contact" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Contact</Link>
           </li>
         </ul>
       </div>
