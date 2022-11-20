910e2a37bc4e10e1aa68d1e30734416119208d0d
Olivier-Kango <olivierkango@gmail.com>
2022-11-19 23:53:40 -0800

    Add content in the accueil page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import Accueil from './components/Accueil';
-import About from './components/About';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Medias from './components/Medias';
 import Formation from './components/Formation';
 import Services from './components/Services';
 import Direct from './components/Direct';
+import Extensions from './components/Extensions';
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Accueil />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/nos-extensions" element={<Extensions />} />
+        <Route path="/nos-programmes" element={<Services />} />
         <Route path="/formation" element={<Formation />} />
-        <Route path="/qui-sommes-nous" element={<About />} />
         <Route path="/medias" element={<Medias />} />
         <Route path="/direct" element={<Direct />} />
         <Route path="/contact" element={<Contact />} />
