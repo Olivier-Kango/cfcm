6186c89448eaccc0d8169bc4056cd5714ddbd34d
Olivier-Kango <olivierkango@gmail.com>
2022-11-19 23:54:37 -0800

    Add cfcm logo in the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.jpg';
+
+const logoss = {
+  width: '12%',
+};
+
+const logos = {
+  width: '100%',
+};
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg bg-white">
     <div className="container-fluid">
-      <Link to="/" className="navbar-brand">
-        <i>C.F.C.M</i>
+      <Link to="/" className="navbar-brand" style={logoss}>
+        <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"><img src={logo} alt="logo_cfcm" style={logos} /></span>
       </Link>
       <button
         className="navbar-toggler"
@@ -23,25 +32,34 @@ const Navbar = () => (
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to="/" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Accueil</Link>
+            <Link to="/nos-extensions" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Nos extensions</span>
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/formation" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Formation</Link>
+            <Link to="/nos-programmes" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Nos Programmes</span>
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/services" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Nos Services</Link>
+            <Link to="/formation" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Formation</span>
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/qui-sommes-nous" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Qui sommes-nous?</Link>
+            <Link to="/medias" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"> Médias</span>
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/medias" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Médias</Link>
+            <Link to="/direct" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"> Direct</span>
+            </Link>
           </li>
-          <li className="nav-item active">
-            <Link to="/direct" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Direct</Link>
-          </li>
-          <li className="nav-item active">
-            <Link to="/contact" className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Contact</Link>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link active">
+              <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Contact</span>
+            </Link>
           </li>
         </ul>
       </div>
