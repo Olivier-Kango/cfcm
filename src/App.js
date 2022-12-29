a8dec624e26ef8c167ff1db63a220d0dcf61d20b
Olivier-Kango <olivierkango@gmail.com>
2022-12-28 19:20:52 -0800

    Change Services in Programmes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Medias from './components/Medias';
 import Formation from './components/Formation';
-import Services from './components/Services';
+import Programmes from './components/Programmes';
 import Direct from './components/Direct';
 import Extensions from './components/Extensions';
 
@@ -20,7 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/nos-extensions" element={<Extensions />} />
-        <Route path="/nos-programmes" element={<Services />} />
+        <Route path="/nos-programmes" element={<Programmes />} />
         <Route path="/formation" element={<Formation />} />
         <Route path="/medias" element={<Medias />} />
         <Route path="/direct" element={<Direct />} />
