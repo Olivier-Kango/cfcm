ede6509fa3a82921f13cf1ee3a06a44018d11479
Olivier-Kango <olivierkango@gmail.com>
2022-12-29 15:42:32 -0800

    Put margin to zero

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -7,13 +7,6 @@ const Accueil = () => (
     <section className="home">
       <div className="div-home">
         <Slider />
-        <p className="home-content">
-          Lorem
-        </p>
-        <p className="date">
-          Ipsum
-        </p>
-        <p className="footer">@ Communauté de la Foi chrétienne dans le Monde, CFCM en sigle. Goma - DRC Congo</p>
       </div>
     </section>
   </div>
