32bcd0a4649d8b14b7689ce602800146c18ad6a9
Olivier-Kango <olivierkango@gmail.com>
2023-01-23 10:14:42 -0800

    Add Accueil

--- src/components/Programmes.js
+++ src/components/Programmes.js
@@ -3,7 +3,23 @@ import '../styling/Programmes.scss';
 
 const Programmes = () => (
   <div className="programmes">
-    Nos Programmes
+    <h2>Nos Programmes</h2>
+    Programmes de l&apos;église
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;1. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;2. Chaque mardi de 7h30 à 9h30&apos; : culte des Mamans
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Enseignements bibliques
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;4. Chaque Samedi de 7h30 à 10h30 : culte des Papas
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;5. Dimanche : de 9h&apos;&apos; à 12h30&apos;&apos;
+    culte d&apos;adoration
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⁃ de 13h&apos;&apos; à 15h30&apos;&apos; culte des jeunes
+    <br />
+    LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
   </div>
 );
 
