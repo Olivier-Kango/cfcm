754aab32aedcaa861d3616668867f3012b93bf6c
Olivier-Kango <olivierkango@gmail.com>
2023-02-04 20:50:55 -0800

    Update programmes

--- src/components/Programmes.js
+++ src/components/Programmes.js
@@ -6,20 +6,24 @@ const Programmes = () => (
     <h2>Nos Programmes</h2>
     Programmes de l&apos;église
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;1. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
+    &nbsp;&nbsp;&nbsp;&nbsp;7. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
     <br />
     &nbsp;&nbsp;&nbsp;&nbsp;2. Chaque mardi de 7h30 à 9h30&apos; : culte des Mamans
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Enseignements bibliques
+    &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Etudes bibliques
     <br />
     &nbsp;&nbsp;&nbsp;&nbsp;4. Chaque Samedi de 7h30 à 10h30 : culte des Papas
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;5. Dimanche : de 9h&apos;&apos; à 12h30&apos;&apos;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    5. Prière d&apos;Intercession de 09h00 à 12h00
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;1. Dimanche : de 9h00&apos; à 12h30&apos;
     culte d&apos;adoration
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⁃ de 13h&apos;&apos; à 15h30&apos;&apos; culte des jeunes
+    &nbsp;&nbsp;&nbsp;
+    6. Chaque Premier et Troisième Dimanche de 13h00&apos; à 14h30&apos; Culte des jeunes
     <br />
-    LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
+    8. LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
   </div>
 );
 
