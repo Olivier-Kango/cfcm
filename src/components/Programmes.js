555450f0dec5c7561d03f6500629257926a875f0
Olivier-Kango <olivierkango@gmail.com>
2023-02-05 22:08:55 -0800

    Add 3

--- src/components/Programmes.js
+++ src/components/Programmes.js
@@ -11,8 +11,6 @@ const Programmes = () => (
     <br />
     &nbsp;&nbsp;&nbsp;&nbsp;2. Chaque mardi de 7h30 à 9h30&apos; : culte des Mamans
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;7. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
-    <br />
     &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Etudes bibliques
     <br />
     &nbsp;&nbsp;&nbsp;&nbsp;4. Chaque Samedi de 7h30 à 10h30 : culte des Papas
@@ -22,6 +20,8 @@ const Programmes = () => (
     <br />
     &nbsp;&nbsp;&nbsp;
     6. Chaque Premier et Troisième Dimanche de 13h00&apos; à 14h30&apos; Culte des jeunes
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;7. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
     <br />
     8. LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
   </div>
