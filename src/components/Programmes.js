d7301afe12399566ef9c25d7b6cbe04503e870a4
Olivier-Kango <olivierkango@gmail.com>
2023-02-11 17:19:22 -0800

    Add Programmes

--- src/components/Programmes.js
+++ src/components/Programmes.js
@@ -3,27 +3,82 @@ import '../styling/Programmes.scss';
 
 const Programmes = () => (
   <div className="programmes">
-    <h2>Nos Programmes</h2>
-    Programmes de l&apos;église
+    <h2>NOS PROGRAMMES</h2>
+    <p>Les Différents Programmes au sein de l&apos;église</p>
     <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;1. Dimanche : de 9h00&apos; à 12h30&apos;
-    culte d&apos;adoration
-    <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;2. Chaque mardi de 7h30 à 9h30&apos; : culte des Mamans
-    <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Etudes bibliques
-    <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;4. Chaque Samedi de 7h30 à 10h30 : culte des Papas
-    <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;
-    5. Prière d&apos;Intercession de 09h00 à 12h00
-    <br />
-    &nbsp;&nbsp;&nbsp;
-    6. Chaque Premier et Troisième Dimanche de 13h00&apos; à 14h30&apos; Culte des jeunes
-    <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;7. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
-    <br />
-    8. LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
+    <div className="programme-details">
+      <div className="programme-item">
+        <h3>DIMANCHE</h3>
+        Culte d&apos;adoration de 9h00 à 12h30
+        <br />
+        (diffusé en Live) 🔴
+      </div>
+      <div className="programme-item">
+        <h3>LUNDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>MARDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>MERCREDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>JEUDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>VENDREDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>SAMEDI</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      <div className="programme-item">
+        <h3>TOUS LES JOURS</h3>
+        A
+        <br />
+        De 17h à 19h30
+      </div>
+      {/* <h2>Nos Programmes</h2>
+      Programmes de l&apos;église
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;1. Dimanche : de 9h00&apos; à 12h30&apos;
+      culte d&apos;adoration
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;2. Chaque mardi de 7h30 à 9h30&apos; : culte des Mamans
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;3. Chaque vendredi de 15h30 à 17h30: Etudes bibliques
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;4. Chaque Samedi de 7h30 à 10h30 : culte des Papas
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      5. Prière d&apos;Intercession de 09h00 à 12h00
+      <br />
+      &nbsp;&nbsp;&nbsp;
+      6. Chaque Premier et Troisième Dimanche de 13h00&apos; à 14h30&apos; Culte des jeunes
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;7. Du lundi au vendredi : de 5h30 à 6h30 : Culte matinal
+      <br />
+      8. LES AFFERMISSEMENTS SE PASSENT TOUS LES JOURS, A TOUT MOMENT SELON VOTRE DISPONIBILITÉ
+      <br /> */}
+      {/* Intercessions point et sous points */}
+    </div>
   </div>
 );
 
