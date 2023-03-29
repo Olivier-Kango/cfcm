2c272e12ec03ca8c051eb7a2f3115a129d255858
Olivier-Kango <olivierkango@gmail.com>
2023-03-29 22:36:41 +0200

    Add empty tag

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -22,76 +22,88 @@ const Accueil = () => {
   return (
     <div className="div-accueil">
       {!imagesLoaded && <LoadingAnimation />}
-      <section className="home">
-        <div className="div-home">
-          <Slider onLoad={handleImageLoad} />
-        </div>
-      </section>
-      <section className="main-program">
-        <div className="div-main-program">
-          <h2>Nous croyons en: </h2>
-          <div className="underline" />
-          <div className="program-flex">
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faPrayingHands} />
-              </i>
-              <h3>Un seul Dieu</h3>
-              <p>Souverain, Tout Puissant et Sage. Créateur des choses visibles et invisibles.</p>
-            </div>
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faCross} />
-              </i>
-              <h3>Jésus Christ</h3>
-              <p>Fils unique du Père qu&apos;il nous a fait connaître, vrai Dieu et vrai Homme.</p>
-            </div>
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faFire} />
-              </i>
-              <h3>Saint-Esprit</h3>
-              <p>Il agit encore aujourd&apos;hui en révélant J-C comme Sauveur et Seigneur.</p>
-            </div>
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faBible} />
-              </i>
-              <h3>La Bible</h3>
-              <p>Composée des écrits canoniques de l&apos;Ancien et Nouveau Testament. (66 livr)</p>
-            </div>
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faWater} />
-              </i>
-              <h3>Au Baptême</h3>
-              <p>Par immersion comme acte de soumission au Seigneur et acquis par tout chrétien.</p>
-            </div>
-            <div className="program">
-              <i className="male-female">
-                <FontAwesomeIcon icon={faMale} />
-                <FontAwesomeIcon icon={faFemale} />
-              </i>
-              <h3>Au Mariage monogamique</h3>
-              <p>Genèse 2: 24, Mat 12: 4 - 6, Hé 13: 4 - 6, Mat 5: 4 - 6, 1 Cor 7: 2 - 11</p>
-            </div>
-            <div className="program">
-              <i>
-                <FontAwesomeIcon icon={faPrayingHands} />
-              </i>
-              <h3>La Sainteté</h3>
-              <p>La Sainteté dans la pensée, dans la parole et dans la conduite.</p>
-            </div>
+      <>
+        <section className="home">
+          <div className="div-home">
+            <Slider onLoad={handleImageLoad} />
           </div>
-          <Link to="/formation">
-            <button type="button">
-              Voir nos Formations
-            </button>
-          </Link>
-          <Link to="/nos-programmes" className="see-whole-program">VOIR TOUS NOS PROGRAMMES</Link>
-        </div>
-      </section>
-      <Speakers />
+        </section>
+        <section className="main-program">
+          <div className="div-main-program">
+            <h2>Nous croyons en: </h2>
+            <div className="underline" />
+            <div className="program-flex">
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faPrayingHands} />
+                </i>
+                <h3>Un seul Dieu</h3>
+                <p>
+                  Souverain, Tout Puissant et Sage. Créateur des choses visibles et invisibles.
+                </p>
+              </div>
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faCross} />
+                </i>
+                <h3>Jésus Christ</h3>
+                <p>
+                  Fils unique du Père qu&apos;il nous a fait connaître, vrai Dieu et vrai Homme.
+                </p>
+              </div>
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faFire} />
+                </i>
+                <h3>Saint-Esprit</h3>
+                <p>Il agit encore aujourd&apos;hui en révélant J-C comme Sauveur et Seigneur.</p>
+              </div>
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faBible} />
+                </i>
+                <h3>La Bible</h3>
+                <p>
+                  Composée des écrits canoniques de l&apos;Ancien et Nouveau Testament.
+                  {' '}
+                  (66 Livres)
+                </p>
+              </div>
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faWater} />
+                </i>
+                <h3>Au Baptême</h3>
+                <p>
+                  Par immersion comme acte de soumission au Seigneur et acquis par tout chrétien.
+                </p>
+              </div>
+              <div className="program">
+                <i className="male-female">
+                  <FontAwesomeIcon icon={faMale} />
+                  <FontAwesomeIcon icon={faFemale} />
+                </i>
+                <h3>Au Mariage monogamique</h3>
+                <p>Genèse 2: 24, Mat 12: 4 - 6, Hé 13: 4 - 6, Mat 5: 4 - 6, 1 Cor 7: 2 - 11</p>
+              </div>
+              <div className="program">
+                <i>
+                  <FontAwesomeIcon icon={faPrayingHands} />
+                </i>
+                <h3>La Sainteté</h3>
+                <p>La Sainteté dans la pensée, dans la parole et dans la conduite.</p>
+              </div>
+            </div>
+            <Link to="/formation">
+              <button type="button">
+                Voir nos Formations
+              </button>
+            </Link>
+            <Link to="/nos-programmes" className="see-whole-program">VOIR TOUS NOS PROGRAMMES</Link>
+          </div>
+        </section>
+        <Speakers />
+      </>
     </div>
   );
 };
