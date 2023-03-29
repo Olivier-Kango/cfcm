71ee13cc6ea47d305f038b6b3e234358a4f0e217
Olivier-Kango <olivierkango@gmail.com>
2023-03-29 21:42:38 +0200

    Add loading animation component

---
+++ src/components/LoadingAnimation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styling/LoadingAnimation.scss';
+
+const LoadingAnimation = () => (
+  <div className="load-wrapper">
+    <h1>Loading...</h1>
+    <ul>
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+    </ul>
+  </div>
+);
+
+export default LoadingAnimation;
