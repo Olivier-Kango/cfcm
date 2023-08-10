8d742266c823e25845e251b0ed75bdbf25ab42c4
Olivier-Kango <olivierkango@gmail.com>
2023-08-10 22:06:38 +0300

    import Footer scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import '../styling/Footer.scss';
 
 const Footer = () => (
   <>
