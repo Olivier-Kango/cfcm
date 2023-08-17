ebb07927a70c2f20e9d423cd40d5da407f830f69
Olivier-Kango <olivierkango@gmail.com>
2023-08-17 13:41:39 +0300

    Add indetation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,46 +12,46 @@ const Footer = () => {
   };
 
   return (
-  <div id="footer">
-    <div className="footer-content">
-      <div className="contact-info">
-        <h4>Nous contacter</h4>
-        <p>
-          <FontAwesomeIcon icon={faEnvelope} />
-          {' '}
-          cfcmgoma@gmail.com
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faWhatsapp} style={whatsappIconStyle} />
-          (+243)995-865-149
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faWhatsapp} style={whatsappIconStyle} />
-          (+243)997-604-877
-        </p>
-      </div>
-      <div className="social-media">
-        <h4>Suivez-nous sur les réseaux sociaux</h4>
-        <div className="social-icons">
-          <a href="https://web.facebook.com/profile.php?id=100082159523264" className="facebook">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a href="https://www.youtube.com/@eglisecfcm-ci5kn" className="youtube">
-            <FontAwesomeIcon icon={faYoutube} />
-          </a>
+    <div id="footer">
+      <div className="footer-content">
+        <div className="contact-info">
+          <h4>Nous contacter</h4>
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} />
+            {' '}
+            cfcmgoma@gmail.com
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faWhatsapp} style={whatsappIconStyle} />
+            (+243)995-865-149
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faWhatsapp} style={whatsappIconStyle} />
+            (+243)997-604-877
+          </p>
+        </div>
+        <div className="social-media">
+          <h4>Suivez-nous sur les réseaux sociaux</h4>
+          <div className="social-icons">
+            <a href="https://web.facebook.com/profile.php?id=100082159523264" className="facebook">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a href="https://www.youtube.com/@eglisecfcm-ci5kn" className="youtube">
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+          </div>
         </div>
       </div>
+      <div className="copyright">
+        <p>
+          &copy;
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          Église C.F.C.M. Tous droits réservés.
+        </p>
+      </div>
     </div>
-    <div className="copyright">
-      <p>
-        &copy;
-        {' '}
-        {new Date().getFullYear()}
-        {' '}
-        Église C.F.C.M. Tous droits réservés.
-      </p>
-    </div>
-  </div>
   );
 }
 
